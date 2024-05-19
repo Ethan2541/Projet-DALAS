0138000ee337f047fa19a7e4c22ab2fed6700f85
Ethan2541 <eluong41@gmail.com>
2024-05-19 18:19:07 +0200

    Add dashboard

---
+++ dashboard.py
@@ -0,0 +1,8 @@
+# TODO
+# Sections : Parties importantes de l'EDA
+# ---
+# Mise en forme conditionnelle
+# Visualisation des données
+# Résultats du clustering
+# ---
+# Paragraphes pour expliquer quelques analyses et résumer légèrement les résultats
