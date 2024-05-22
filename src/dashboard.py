5dfbe749b8f04cd4c32e9371759be8da24ef9c22
Ethan2541 <eluong41@gmail.com>
2024-05-22 12:05:21 +0200

    Models + Figures

--- src/dashboard.py
+++ src/dashboard.py
@@ -13,6 +13,19 @@ import pandas as pd
 import plotly.express as px
 
 
+df = pd.read_csv("./../data/goodreads_data.csv", converters={"genres": lambda x: x[1:-1].split(",")})
+df["publication_date"] = pd.to_datetime(df["published"].apply(lambda x: " ".join(str(x).split()[:3])), errors="coerce")
+df["publication_year"] = df["publication_date"].dt.year.astype("float")
+df["n_pages"] = df["pages_format"].apply(lambda x: int(x.split()[0]) if x.split()[0].isdigit() else np.nan).astype("float")
+df["format"] = df["pages_format"].apply(lambda x: x.replace("Kindle Edition", "Kindle").split()[-1].lower() if x.replace("Kindle Edition", "Kindle").split()[-1].lower() in ["hardcover", "paperback", "ebook", "audiobook", "audio", "cd", "kindle"] else "other").astype("category")
+
+dic_rating = {1: "one_star", 2: "two_stars", 3: "three_stars", 4: "four_stars", 5: "five_stars"}
+for key, value in dic_rating.items():
+    df[value] = df["ratings"].apply(lambda x: float(x[1:-1].split(",")[key-1])) / df["n_ratings"]
+df["average_rating"] = df["ratings"].apply(lambda x: sum([(i+1) * float(x[1:-1].split(",")[i]) for i in range(5)])) / df["n_ratings"]
+
+
+
 app = Dash(__name__)
 
 app.layout = html.Div([
@@ -25,10 +38,10 @@ app.layout = html.Div([
     ], style={'background-color': '#f5f6f8', 'padding': '20px', 'border-radius': '5px', 'margin-bottom': '30px'}),
     html.Div([
         html.H2(children='Exploration des Données', style={'text-align': 'center', 'margin': '0', 'color': '#2a303b'}),
-        dcc.Graph(figure=px.bar(df_bar, x='bins', y='freq', labels={'bins': 'Catégorie de Revenu', 'freq': 'Proportion'}, title='Médiane des revenus selon la localisation géographique'), id='freq-income-bar'),
+        dcc.Graph(figure=px.bar(df, x='bins', y='freq', labels={'bins': 'Catégorie de Revenu', 'freq': 'Proportion'}, title='Médiane des revenus selon la localisation géographique'), id='freq-income-bar'),
         html.Div([
             dcc.Graph(figure=px.histogram(df, x='winner', y='votes_gop', labels={'winner': 'Parti gagnant', 'votes_gop': 'Nombre de votes'}, title='Résultats des élections'), id='votes-gop-hist'),
-            dcc.Graph(figure=px.bar(df_bar2, x='bins', y='freq', labels={'bins': 'Catégorie selon le nombre de votes', 'freq': 'Proportion des votes'}, title='Répartition des voix selon la localisation'), id='zipf-bar')
+            dcc.Graph(figure=px.bar(df, x='bins', y='freq', labels={'bins': 'Catégorie selon le nombre de votes', 'freq': 'Proportion des votes'}, title='Répartition des voix selon la localisation'), id='zipf-bar')
         ], style={'display': 'grid', 'grid-template-columns': '1fr 2fr', 'gap': '10px'})
     ], style={'padding': '20px', 'border-radius': '5px'})
 ], style={'font-family': 'Helvetica', 'background-color': 'white', 'padding': '10px', 'border-radius': '5px', 'margin-bottom': '10px'})
@@ -41,7 +54,7 @@ def update_table(mode):
     if mode == 'Résumé':
         return df.describe().to_dict('records')
     elif mode == 'Résumé (normalisé)':
-        return df_norm.describe().to_dict('records')
+        return df.describe().to_dict('records')
     elif mode == 'Données':
         return df.to_dict('records')
     
